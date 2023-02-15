8139f66f5885364b2d72e86b5e624f1f4e64d782
Tejas <jtjtejas05@gmail.com>
2023-02-15 08:07:07 +0530

    Updated exp 01; ;

--- Experiment 01/code/1b_sort_analysis.cpp
+++ Experiment 01/code/1b_sort_analysis.cpp
@@ -1,6 +1,14 @@
+#include <stdio.h>
+
 #include <chrono>
+#include <cmath>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
+using namespace std;
+
+double insertion_comparision = 0.0;
+double selection_comparision = 0.0;
 
 void insertion_sort(int* arr, int size) {
     for (int i = 1; i < size; i++) {
@@ -9,6 +17,7 @@ void insertion_sort(int* arr, int size) {
         while (j >= 0 && current < arr[j]) {
             arr[j + 1] = arr[j];
             j--;
+            insertion_comparision++;
         }
         arr[j + 1] = current;
     }
@@ -24,6 +33,7 @@ void selection_sort(int* arr, int size) {
     for (int i = 0; i < size - 1; i++) {
         int i_min = i;
         for (int j = i + 1; j < size; j++) {
+            selection_comparision++;
             if (arr[j] < arr[i_min]) {
                 i_min = j;
             }
@@ -34,11 +44,15 @@ void selection_sort(int* arr, int size) {
     }
 }
 
+int digits(int num) {
+    return num == 0 ? 1 : floor(log10(abs(num))) + 1;
+}
+
 int main() {
-    int* arr_ins = new int[100000];
-    int* arr_sel = new int[100000];
-    std::ifstream nums("random_numbers.txt");
-    std::ofstream output("../csv/sort_analysis.csv");
+    int arr_ins[100000];
+    int arr_sel[100000];
+    ifstream nums("random_numbers.txt");
+    ofstream output("../csv/sort_analysis.csv");
     output << "block_size,insertion,selection\n";
 
     for (int i = 1; i <= 100000; i++) {
@@ -50,24 +64,37 @@ int main() {
 
     // 1000 blocks of 100 numbers
     for (int i = 1; i <= 1000; i++) {
-        auto ins_start = std::chrono::high_resolution_clock::now();
+        // print 10 values at index 10000, 20000, ...
+        int index = i * 100;
+        if (index % 10000 == 0 && index != 100000) {
+            cout << "\nPrinting 10 values from index " << index << endl;
+            for (int t = 0; t < 10; t++) {
+                cout << index + t << " : " << arr_ins[index + t] << "\n";
+            }
+        }
+
+        // insertion
+        auto ins_start = chrono::high_resolution_clock::now();
         insertion_sort(arr_ins, i * 100);
-        auto ins_end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> ins_time = (ins_end - ins_start);
+        auto ins_end = chrono::high_resolution_clock::now();
+        chrono::duration<double> ins_time = (ins_end - ins_start);
 
-        auto sel_start = std::chrono::high_resolution_clock::now();
+        // selection
+        auto sel_start = chrono::high_resolution_clock::now();
         selection_sort(arr_sel, i * 100);
-        auto sel_end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> sel_time = (sel_end - sel_start);
-
-        // std::cout << "\nBlock : " << i * 100 << "\n";
-        // std::cout << "insertion sort : " << ins_time.count() << "\t"
-        //           << "selection sort : " << sel_time.count() << "\n";
+        auto sel_end = chrono::high_resolution_clock::now();
+        chrono::duration<double> sel_time = (sel_end - sel_start);
 
         output << i * 100 << "," << ins_time.count() << "," << sel_time.count() << "\n";
     }
 
-    std::cout << "Sorting completed !";
+    cout << "Sorting completed !" << endl;
+
+    cout << "\nSmallest Number = " << arr_ins[0] << "\tDigits = " << digits(arr_ins[0]) << endl;
+    cout << "Largest Number = " << arr_ins[99999] << "\tDigits = " << digits(arr_ins[99999]) << endl;
+
+    printf("Insertion sort comparision count: %.0lf\n", insertion_comparision);
+    printf("Selection sort comparision count: %.0lf\n", selection_comparision);
 
     return 0;
 }
--- Experiment 02/code/sort_analysis.cpp
+++ Experiment 02/code/sort_analysis.cpp
@@ -1,8 +1,12 @@
 #include <bits/stdc++.h>
+#include <stdio.h>
 
 #include <chrono>
 #include <fstream>
 using namespace std;
+
+double merge_comparision = 0.0;
+double quick_comparision = 0.0;
 
 void print_array(int* a, int size) {
     for (int i = 0; i < size; i++) {
@@ -33,14 +37,17 @@ void merge(int* arr, int l, int m, int h) {
 
     while (x < left_size && y < right_size) {
         arr[z++] = left[x] <= right[y] ? left[x++] : right[y++];
+        merge_comparision++;
     }
 
     while (x < left_size) {
         arr[z++] = left[x++];
+        merge_comparision++;
     }
 
     while (y < right_size) {
         arr[z++] = right[y++];
+        merge_comparision++;
     }
 }
 
@@ -67,14 +74,17 @@ int partition(int* a, int l, int h) {
 
     while (low < high) {
         while (a[low] <= a[pivot] && low < h) {
+            quick_comparision++;
             low++;
         }
 
         while (a[high] > a[pivot] && high >= l) {
+            quick_comparision++;
             high--;
         }
 
         if (low < high) {
+            quick_comparision++;
             swap(a[low], a[high]);
         }
     }
@@ -93,6 +103,10 @@ void quick_sort(int* a, int l, int h) {
     }
 }
 
+int digits(int num) {
+    return num == 0 ? 1 : floor(log10(abs(num))) + 1;
+}
+
 int main() {
     int arr_mer[100000];
     int arr_qui[100000];
@@ -107,6 +121,15 @@ int main() {
     }
 
     for (int i = 1; i <= 1000; i++) {
+        // print 10 values at index 10000, 20000, ...
+        int index = i * 100;
+        if (index % 10000 == 0 && index != 100000) {
+            cout << "\nPrinting 10 values from index " << index << endl;
+            for (int t = 0; t < 10; t++) {
+                cout << index + t << " : " << arr_mer[index + t] << "\n";
+            }
+        }
+
         // merge sort
         auto merge_start = chrono::high_resolution_clock::now();
         merge_sort(arr_mer, 0, i * 100 - 1);
@@ -123,7 +146,13 @@ int main() {
                << quick_time.count() << "\n";
     }
 
-    cout << "Sorting completed !" << endl;
+    cout << "\nSorting completed !" << endl;
+
+    cout << "\nSmallest Number = " << arr_mer[0] << "\tDigits = " << digits(arr_mer[0]) << endl;
+    cout << "Largest Number = " << arr_mer[99999] << "\tDigits = " << digits(arr_mer[99999]) << endl;
+
+    printf("Merge sort comparision count: %.0lf\n", merge_comparision);
+    printf("Quick sort comparision count: %.0lf\n", quick_comparision);
 
     return 0;
 }
